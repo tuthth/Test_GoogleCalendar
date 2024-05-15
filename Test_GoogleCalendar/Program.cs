5017c7fea8282242f649e0f9ab1ebded83dd5f68
Anh Tuan <tuanthanthaii@gmail.com>
2024-05-15 12:34:02 +0700

    add tips

--- Test_GoogleCalendar/Program.cs
+++ Test_GoogleCalendar/Program.cs
@@ -25,19 +25,20 @@ public class Program
             HttpClientInitializer = credential
         });
 
+        //Có thể lược bỏ từ dòng này cho đến dòng 38 nếu không muốn tạo 1 calendar mới hoặc sử dụng calendar đã có sẵn
         // Tạo một lịch mới
-        var newCalendar = new Google.Apis.Calendar.v3.Data.Calendar();
-        newCalendar.Summary = "Test Calendar";
-        var createdCalendar = await service.Calendars.Insert(newCalendar).ExecuteAsync();
+        //var newCalendar = new Google.Apis.Calendar.v3.Data.Calendar();
+        //newCalendar.Summary = "Test Calendar";
+        //var createdCalendar = await service.Calendars.Insert(newCalendar).ExecuteAsync();
 
         // Thêm lịch mới vào danh sách lịch
-        var calendarListEntry = new Google.Apis.Calendar.v3.Data.CalendarListEntry();
-        calendarListEntry.Id = createdCalendar.Id;
-        await service.CalendarList.Insert(calendarListEntry).ExecuteAsync();
+        //var calendarListEntry = new Google.Apis.Calendar.v3.Data.CalendarListEntry();
+        //calendarListEntry.Id = createdCalendar.Id;
+        //await service.CalendarList.Insert(calendarListEntry).ExecuteAsync();
 
         Event calendarEvent = new Event()
         {
-            Summary = "Họp mặt trực tuyến",
+            Summary = "Họp mặt trực tuyến 10h37",
             Location = "Google Meet",
             Description = "Cuộc họp trực tuyến qua Google Meet",
             Start = new EventDateTime()
@@ -71,8 +72,10 @@ public class Program
                 new EventAttendee() { Email = "riconduongcung@gmail.com" }
             }
         };
-
-        EventsResource.InsertRequest request = service.Events.Insert(calendarEvent, createdCalendar.Id);
+        //event với lịch được tạo ở dòng 30-37
+        //EventsResource.InsertRequest request = service.Events.Insert(calendarEvent, createdCalendar.Id);
+        //event với lịch có ID sẵn
+        EventsResource.InsertRequest request = service.Events.Insert(calendarEvent, "your-calendar-id");
         request.ConferenceDataVersion = 1;
         Event createdEvent = await request.ExecuteAsync();
 
